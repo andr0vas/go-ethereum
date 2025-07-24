f550b9c13755ae1a1ec9d11432acc609082ef46f
Andrey Ivanov <andrey1vanov@yahoo.com>
2025-07-24 18:40:35 +0300

    add private key keeper for hiding private key from lib users

---
+++ keeper/keeper.go
@@ -0,0 +1,66 @@
+package keeper
+
+import (
+	"crypto/ecdsa"
+	"errors"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// PrivateKeyKeeper is layer for protecting private key from direct using.
+type PrivateKeyKeeper interface {
+	// GeneratePrivateKey return identifier of new generated private key
+	GeneratePrivateKey() (prvID []byte, err error)
+	// GetPublicKey return public key by private key ID
+	GetPublicKey(prvID []byte) ([]byte, error)
+	// Sign of data by private key ID
+	Sign(data []byte, prvID []byte) ([]byte, error)
+}
+
+// Default realized interface PrivateKeyKeeper without hiding the private key
+var Default PrivateKeyKeeper = &defaultPrivateKeyKeeper{}
+
+type defaultPrivateKeyKeeper struct {
+}
+
+func (a *defaultPrivateKeyKeeper) GeneratePrivateKey() ([]byte, error) {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		return nil, err
+	}
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	return privateKeyBytes, nil
+}
+
+func (a *defaultPrivateKeyKeeper) GetPublicKey(prvID []byte) ([]byte, error) {
+	privateKey, err := crypto.ToECDSA(prvID)
+	if err != nil {
+		return nil, err
+	}
+	publicKeyECDSA, ok := privateKey.Public().(*ecdsa.PublicKey)
+	if !ok {
+		return nil, errors.New("cannot cast public key to ecsda public key")
+	}
+	return crypto.FromECDSAPub(publicKeyECDSA), nil
+}
+
+func (a *defaultPrivateKeyKeeper) Sign(data []byte, prvID []byte) ([]byte, error) {
+	prv, err := crypto.ToECDSA(prvID)
+	if err != nil {
+		return nil, err
+	}
+	sig, err := crypto.Sign(data, prv)
+	if err != nil {
+		return nil, err
+	}
+	return sig, nil
+}
+
+func SignByAdapter(tx *types.Transaction, s types.Signer, prvID []byte) (*types.Transaction, error) {
+	h := s.Hash(tx)
+	sig, err := Default.Sign(h[:], prvID)
+	if err != nil {
+		return nil, err
+	}
+	return tx.WithSignature(s, sig)
+}
